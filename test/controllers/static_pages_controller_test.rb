5345948d616e2291528ba64bcf6c328648945c7a
mugi88 <m211097268+1@gmail.com>
2019-11-15 21:51:03 +0900

    2019/11/15_21:50

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
 
   test "should get home" do
     get root_path
@@ -11,17 +9,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get help" do
-    get static_pages_help_url
     get help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
-  end
     get about_path
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
